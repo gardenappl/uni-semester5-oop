fd3b64d250034dfebe4d5574a23b1ed6e4adef4b
gardenapple <gardenapple@posteo.net>
2020-11-12 22:18:13 +0200

    task1: Unit tests using Mockito

---
+++ task1/client/src/test/java/ua/yuriih/task1/SerializationClientTest.java
@@ -0,0 +1,47 @@
+package ua.yuriih.task1;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class SerializationClientTest {
+    @Test
+    void tryRead() throws Exception {
+        ExampleObject exampleObject = new ExampleObject(1, "2", 3);
+        
+        ExampleObject receivedObject = tryRead_doTestObject(exampleObject);
+
+        assertEquals(exampleObject.getExampleString(), receivedObject.getExampleString());
+        assertEquals(exampleObject.getExampleInt(), receivedObject.getExampleInt());
+        //otherInt doesn't need to be equal
+    }
+
+    @Test
+    void tryRead_fail() throws Exception {
+        String notExampleObject = "Not an ExampleObject instance, this should crash";
+
+        assertThrows(ClassCastException.class, () -> 
+                tryRead_doTestObject(notExampleObject)
+        );
+    }
+    
+    ExampleObject tryRead_doTestObject(Object exampleObject) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(exampleObject);
+
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+
+        
+        Socket socket = mock(Socket.class);
+        when(socket.getInputStream()).thenReturn(byteArrayInputStream);
+        
+        
+        return new SerializationClient(socket).tryRead();
+    }
+}
--- task1/server/src/test/java/ua/yuriih/task1/SerializationServerTest.java
+++ task1/server/src/test/java/ua/yuriih/task1/SerializationServerTest.java
@@ -1,18 +1,51 @@
 package ua.yuriih.task1;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class SerializationServerTest {
     @Test
-    public void serializationServerCreateNonNull() {
+    public void createNonNull() {
         assertThrows(IllegalArgumentException.class, () ->
                 new SerializationServer(new ServerSocket(0), null)
         );
+    }
+
+    @Test
+    public void run() throws Exception {
+        ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
+
+        Socket socket = mock(Socket.class);
+        when(socket.getOutputStream()).thenReturn(arrayOutputStream);
+
+        ServerSocket serverSocket = mock(ServerSocket.class);
+        when(serverSocket.accept()).thenReturn(socket);
+
+
+        ExampleObject exampleObject = new ExampleObject(1, "Example2", 3);
+
+        new SerializationServer(serverSocket, exampleObject).run();
+
+
+        ByteArrayInputStream arrayInputStream = new ByteArrayInputStream(arrayOutputStream.toByteArray());
+        ObjectInputStream objectInputStream = new ObjectInputStream(arrayInputStream);
+
+        ExampleObject sentObject = (ExampleObject)objectInputStream.readObject();
+        assertEquals(exampleObject.getExampleInt(), sentObject.getExampleInt());
+        assertEquals(exampleObject.getExampleString(), sentObject.getExampleString());
+        //otherInt doesn't need to be equal
     }
 }
