06bddccc1b5cf97a0e6ae33c101e7c1b558c86f5
gardenapple <gardenapple@posteo.net>
2020-11-23 12:25:20 +0200

    task6: Implemented Michael-Schoot non-blocking queue

---
+++ task6-nonblocking-queue/src/main/java/ua/yuriih/task6/ConcurrentQueue.java
@@ -0,0 +1,75 @@
+package ua.yuriih.task6;
+
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+public class ConcurrentQueue<T> {
+    private static final class Node<T> {
+        public final AtomicReference<Node<T>> next;
+        public final T value;
+
+        Node(Node<T> next, T value) {
+            this.next = new AtomicReference<>(next);
+            this.value = value;
+        }
+    }
+
+    private AtomicReference<Node<T>> head;
+    private AtomicReference<Node<T>> tail;
+
+    public ConcurrentQueue() {
+        Node<T> dummy = new Node<>(null, null);
+        head = new AtomicReference<>(dummy);
+        tail = new AtomicReference<>(dummy);
+    }
+
+    public void enqueue(T value) {
+        Node<T> node = new Node<>(null, value);
+        AtomicReference<Node<T>> currentTail;
+        while (true) {
+            currentTail = tail;
+            AtomicReference<Node<T>> next = currentTail.get().next;
+
+            if (currentTail.get() == tail.get()) {
+                if (next.get() == null) {
+                    if (currentTail.get().next.compareAndSet(null, node))
+                        break;
+                } else {
+                    //Tail was not pointing to the last node,
+                    //try to swing tail to the next node
+                    tail.compareAndSet(currentTail.get(), next.get());
+                }
+            }
+        }
+        //Done, try to swing tail to the inserted node
+        tail.compareAndSet(currentTail.get(), node);
+    }
+
+    public T dequeue() {
+        T returnedValue;
+
+        while (true) {
+            AtomicReference<Node<T>> currentHead = head;
+            AtomicReference<Node<T>> currentTail = tail;
+            AtomicReference<Node<T>> next = currentHead.get().next;
+
+            if (currentHead.get() == head.get()) {
+                //is queue empty or is tail falling behind?
+                if (currentHead.get() == tail.get()) {
+                    if (next.get() == null) {
+                        //Queue is empty
+                        return null;
+                    }
+                    //Tail is falling behind, advance it
+                    tail.compareAndSet(currentTail.get(), next.get());
+                } else {
+                    returnedValue = next.get().value;
+                    //Try to swing Head to the next node
+                    if (head.compareAndSet(currentHead.get(), next.get()))
+                        break;
+                }
+            }
+        }
+        return returnedValue;
+    }
+}
