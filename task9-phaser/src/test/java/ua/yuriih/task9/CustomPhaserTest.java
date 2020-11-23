58fe5b1fc3d8a90ece43c07e5386b424e869fbaa
gardenapple <gardenapple@posteo.net>
2020-11-23 18:05:13 +0200

    task9: Remake tests using Callables

--- task9-phaser/src/test/java/ua/yuriih/task9/CustomPhaserTest.java
+++ task9-phaser/src/test/java/ua/yuriih/task9/CustomPhaserTest.java
@@ -2,6 +2,10 @@ package ua.yuriih.task9;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomPhaserTest {
@@ -19,6 +23,9 @@ class CustomPhaserTest {
         }
 
         assertEquals(1, phaser.getPhase());
+        assertEquals(0, phaser.getArrivedParties());
+        assertEquals(5, phaser.getUnarrivedParties());
+        assertEquals(5, phaser.getRegisteredParties());
     }
 
     @Test
@@ -32,30 +39,31 @@ class CustomPhaserTest {
     }
 
     @Test
-    public void await_multiThreaded() {
+    public void await_multiThreaded() throws InterruptedException{
         CustomPhaser phaser = new CustomPhaser();
         phaser.bulkRegister(3);
 
-        Thread thread1 = new Thread(() ->
+        //Await on main thread + 2 threads
+
+        ExecutorService service = Executors.newFixedThreadPool(2);
+
+        Future<?> future1 = service.submit(() ->
                 phaser.awaitAdvance(0)
         );
-        Thread thread2 = new Thread(() ->
+        Future<?> future2 = service.submit(() ->
                 phaser.awaitAdvance(0)
         );
 
-        thread1.start();
-        thread2.start();
 
-
-        assertDoesNotThrow(() -> Thread.sleep(500));
+        Thread.sleep(500);
         assertEquals(2, phaser.getArrivedParties());
         assertEquals(1, phaser.getUnarrivedParties());
 
         assertEquals(1, phaser.awaitAdvance(0));
 
-
-        assertDoesNotThrow(() -> thread1.join());
-        assertDoesNotThrow(() -> thread2.join());
+        //Join threads
+        assertDoesNotThrow(() -> future1.get());
+        assertDoesNotThrow(() -> future2.get());
 
 
         assertEquals(1, phaser.getPhase());
@@ -63,22 +71,23 @@ class CustomPhaserTest {
     }
 
     @Test
-    public void await_multiThreaded_unregister() {
+    public void await_multiThreaded_unregister() throws InterruptedException {
         CustomPhaser phaser = new CustomPhaser();
         phaser.bulkRegister(3);
 
-        Thread thread1 = new Thread(() ->
+        //Await on 2 threads, main thread will arrive and deregister
+
+        ExecutorService service = Executors.newFixedThreadPool(2);
+
+        Future<?> future1 = service.submit(() ->
                 phaser.awaitAdvance(0)
         );
-        Thread thread2 = new Thread(() ->
+        Future<?> future2 = service.submit(() ->
                 phaser.awaitAdvance(0)
         );
 
-        thread1.start();
-        thread2.start();
+        Thread.sleep(500);
 
-
-        assertDoesNotThrow(() -> Thread.sleep(500));
         assertEquals(0, phaser.getPhase());
         assertEquals(2, phaser.getArrivedParties());
         assertEquals(1, phaser.getUnarrivedParties());
@@ -86,13 +95,16 @@ class CustomPhaserTest {
         assertEquals(0, phaser.arriveAndDeregister());
         assertEquals(1, phaser.getPhase());
 
-        assertDoesNotThrow(() -> thread1.join());
-        assertDoesNotThrow(() -> thread2.join());
+        //Join threads
+        assertDoesNotThrow(() -> future1.get());
+        assertDoesNotThrow(() -> future2.get());
 
 
         assertEquals(1, phaser.getPhase());
         assertEquals(0, phaser.getArrivedParties());
         assertEquals(2, phaser.getRegisteredParties());
+
+        //De-register until no more registered parties left
 
         assertEquals(1, phaser.arriveAndDeregister());
         assertEquals(1, phaser.arriveAndDeregister());
