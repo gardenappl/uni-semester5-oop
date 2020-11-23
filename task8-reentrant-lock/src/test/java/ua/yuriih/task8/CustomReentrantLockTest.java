f94a1e00957ae10fc721ddc4b89b6d3ef4882b86
gardenapple <gardenapple@posteo.net>
2020-11-23 17:13:11 +0200

    task8: remake tests using Callables

--- task8-reentrant-lock/src/test/java/ua/yuriih/task8/CustomReentrantLockTest.java
+++ task8-reentrant-lock/src/test/java/ua/yuriih/task8/CustomReentrantLockTest.java
@@ -2,6 +2,9 @@ package ua.yuriih.task8;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,17 +17,20 @@ class CustomReentrantLockTest {
         assertTrue(lock.tryLock());
         lock.unlock();
 
-        Thread otherThread = new Thread(() -> {
+        ExecutorService service = Executors.newSingleThreadExecutor();
+
+        Future<?> future = service.submit(() -> {
             lock.lock();
             assertDoesNotThrow(() -> Thread.sleep(1000));
             lock.unlock();
         });
-        otherThread.start();
 
         for (int i = 0; i < 9; i++) {
             Thread.sleep(100);
             assertFalse(lock.tryLock());
         }
+
+        assertDoesNotThrow(() -> future.get());
 
         Thread.sleep(500);
         assertTrue(lock.tryLock());
@@ -47,42 +53,45 @@ class CustomReentrantLockTest {
     }
 
     @Test
-    void lock_interruptibly() throws InterruptedException {
+    void lock_interruptibly() {
         CustomReentrantLock lock = new CustomReentrantLock();
 
-        lock.lock();
 
-        Thread otherThread = new Thread(() -> {
+        ExecutorService service = Executors.newSingleThreadExecutor();
+
+        Thread mainThread = Thread.currentThread();
+
+        Future<?> future = service.submit(() -> {
             lock.lock();
-            assertThrows(InterruptedException.class, () -> lock.lockInterruptibly());
+
+            assertDoesNotThrow(() -> Thread.sleep(1000));
+            mainThread.interrupt();
+            assertDoesNotThrow(() -> Thread.sleep(100));
+
             lock.unlock();
         });
-        otherThread.start();
 
-        Thread.sleep(100);
-        otherThread.interrupt();
-
-        lock.unlock();
+        assertDoesNotThrow(() -> Thread.sleep(500));
+        assertThrows(InterruptedException.class, () -> lock.lockInterruptibly());
+        assertDoesNotThrow(() -> future.get());
     }
 
     @Test
     void tryLock_timeout() throws InterruptedException {
         CustomReentrantLock lock = new CustomReentrantLock();
 
-
-        Thread otherThread = new Thread(() -> {
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        Future<?> future = service.submit(() -> {
             lock.lock();
             assertDoesNotThrow(() -> Thread.sleep(1000));
             lock.unlock();
         });
-        otherThread.start();
 
         Thread.sleep(100);
         
         assertFalse(lock.tryLock(100, TimeUnit.MILLISECONDS));
 
-        otherThread.join();
-
+        assertDoesNotThrow(() -> future.get());
         
         assertTrue(lock.tryLock(100, TimeUnit.MILLISECONDS));
         lock.unlock();
